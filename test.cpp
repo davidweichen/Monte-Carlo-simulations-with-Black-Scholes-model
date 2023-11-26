ddc8114ce57e78c16dcb462d62eb61fd2745e7c9
davidweichen <david19990212@gmail.com>
2023-11-26 15:49:32 -0800

    testing

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <random>
+
 #include <cmath>
 #include <omp.h>
 #include <chrono>
@@ -12,11 +12,21 @@ int main() {
     int b = 10;
     vector<int> v(num_particles);
     chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
-    #pragma omp parallel for
+    #pragma omp parallel
+    {
+      int thread_id = omp_get_thread_num();
+      // Number of threads in the current team
+      int nthreads = omp_get_num_threads();
+
+    #pragma omp critical
+      {
+         std::cout << "Hello world, I'm thread " << thread_id << " out of " << nthreads << " total threads. " << std::endl; 
+      }
+    #pragma omp for
         for (int i = 0; i < num_particles; i++) {
             v[i] = a + b;
         }    
-    
+    }
     chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end_time - start_time;
     cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
