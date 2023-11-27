2da2c1be06116e832608905dc08ca0e735ca2b7c
davidweichen <david19990212@gmail.com>
2023-11-26 16:40:18 -0800

    testing

--- test.cpp
+++ test.cpp
@@ -8,10 +8,14 @@ int main()
     
     int *arr = new int[ARRAY_SIZE];
     std::fill_n(arr, ARRAY_SIZE, ARRAY_VALUE);
+    chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
     #pragma omp parallel for
     for(int i = 0; i < ARRAY_SIZE; i++)
     {
         arr[i] = arr[i] / arr[i] + arr[i] / 5 - 14;
     }
+    chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end_time - start_time;
+    cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
     return 0;
 }
