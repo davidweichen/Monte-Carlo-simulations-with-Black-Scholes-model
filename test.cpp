24fa71ffae17f64307dbfefa02963b26c303e885
davidweichen <david19990212@gmail.com>
2023-12-02 17:21:39 -0800

    new test

--- test.cpp
+++ test.cpp
@@ -20,7 +20,7 @@ double generate_spot_prices(int num_particles, int num_weeks, double strike_pric
     #pragma omp parallel
     {
     // Simulate the spot price at each time step in parallel.
-    #pragma omp for reduction(+:C)
+    #pragma omp for 
     for (int t = 0; t < num_particles; t++) {
         
         spot_prices[t][0] = spot_price;
